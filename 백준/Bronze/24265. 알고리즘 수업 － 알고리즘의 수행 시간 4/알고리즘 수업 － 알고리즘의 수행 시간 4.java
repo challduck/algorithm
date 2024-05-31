fcccb0e1c5b530111617599d224844c929fb31bd
Challduck <123526228+challduck@users.noreply.github.com>
2024-05-31 18:46:18 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Long n = Long.parseLong(reader.readLine());
+
+        System.out.println((n * (n - 1)) / 2);
+        System.out.println(2);
+    }
+}
