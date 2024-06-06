460bdbeb91f853b0cd15ddc51ba294f784f2dc92
Challduck <123526228+challduck@users.noreply.github.com>
2024-06-06 16:01:26 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14372 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,18 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        int answer = 0;
+        BigInteger sum = new BigInteger("0");
+        BigInteger square = new BigInteger("1");
         for (int i = 0; i < n; i++) {
-            answer += (str.charAt(i) - 'a' + 1) * Math.pow(31, i);
+            sum = sum.add(BigInteger.valueOf(str.charAt(i) - 'a' + 1).multiply(square));
+            square = square.multiply(BigInteger.valueOf(31));
         }
-        System.out.println(answer);
+        System.out.println(sum.mod(BigInteger.valueOf(1234567891)));
     }
 }
