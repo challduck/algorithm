855fcdda27e37ac1c7a13702d3ccc8eb68a73536
Challduck <123526228+challduck@users.noreply.github.com>
2024-06-07 23:31:06 +0900

    [Bronze III] Title: 타슈, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/30018. 타슈/타슈.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            sum += Math.abs(Integer.parseInt(st1.nextToken()) - Integer.parseInt(st2.nextToken()));
+        }
+        System.out.println(sum / 2);
+    }
+}
