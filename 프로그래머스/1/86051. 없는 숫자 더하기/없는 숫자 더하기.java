32a0c9e27c02f46aba51a4274159ec33ff247d63
Challduck <123526228+challduck@users.noreply.github.com>
2024-06-12 17:03:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            set.add(numbers[i]);
+        }
+        int answer = 0;
+
+        for (int i = 0; i < 10; i++) {
+            if (!set.contains(i)) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
