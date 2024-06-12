f84d782b82d57ac30f506a822b8646dc1bb2cd8d
Challduck <123526228+challduck@users.noreply.github.com>
2024-06-12 16:53:31 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.51 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < score.length; i++) {
+            if (pq.size() < k){
+                pq.add(score[i]);
+            } else {
+                if (pq.peek() < score[i]) {
+                    pq.add(score[i]);
+                    pq.poll();
+                }
+            }
+            answer[i] = pq.peek();
+        }
+        return answer;
+    }
+}
