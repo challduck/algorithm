a61dfe1ac6c033b4dc653b0a6784ef12127f5010
Challduck <123526228+challduck@users.noreply.github.com>
2024-06-20 22:09:16 +0900

    [Silver IV] Title: 스택, Time: 200 ms, Memory: 19220 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            if (str.equals("push")) {
+                stack.add(Integer.parseInt(st.nextToken()));
+            } else if (str.equals("pop")) {
+                if (!stack.isEmpty()) {
+                    sb.append(stack.pop()).append("\n");
+                } else {
+                    sb.append(-1).append("\n");
+                }
+            } else if (str.equals("size")) {
+                sb.append(stack.size()).append("\n");
+            } else if (str.equals("empty")) {
+                if (stack.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if (str.equals("top")) {
+                if (!stack.isEmpty()) {
+                    sb.append(stack.peek()).append("\n");
+                } else {
+                    sb.append(-1).append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
