ab83d7d9ff29e83f5bc47643074922d0c641b2af
Challduck <dgs2288@naver.com>
2024-07-08 21:10:26 +0900

    [Silver III] Title: 피보나치 함수, Time: 144 ms, Memory: 16180 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[] zero = new int[41];
+    static int[] one = new int[41];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        fibonacci();
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(zero[n] + " " + one[n]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void fibonacci() {
+        zero[0] = 1;
+        one[0] = 0;
+        // zero[1] = 0;
+        // one[1] = 1;
+
+        for (int i = 1; i <= 40; i++) {
+            // zero[i] = zero[i - 1] + zero[i - 2];
+            zero[i] = one[i - 1];
+            // one[i] = one[i - 1] + one[i - 2];
+            one[i] = zero[i - 1] + zero[i];
+        }
+    }
+}
