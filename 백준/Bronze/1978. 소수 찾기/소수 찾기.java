a92d2f08a59775b8b9907a40224dcd0b34028c80
Challduck <dgs2288@naver.com>
2024-07-11 13:26:25 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (isPrime(Integer.parseInt(st.nextToken()))) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
