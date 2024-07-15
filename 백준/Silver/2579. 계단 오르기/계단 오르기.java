08cd2fa08b6642b879ec175df2eb8143f98e8eee
Challduck <dgs2288@naver.com>
2024-07-15 14:50:13 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] score = new int[n];
+        for (int i = 0; i < n; i++) {
+            score[i] = Integer.parseInt(br.readLine());
+        }
+
+        if (n == 1) {
+            System.out.println(score[0]);
+            return;
+        }
+
+        int[] dp = new int[n];
+        dp[0] = score[0];
+        dp[1] = score[0] + score[1];
+
+        if (n > 2) {
+            dp[2] = Math.max(score[0] + score[2], score[1] + score[2]);
+            for (int i = 3; i < n; i++) {
+                dp[i] = Math.max(dp[i - 2], dp[i - 3] + score[i - 1]) + score[i];
+            }
+        }
+        System.out.println(dp[n - 1]);
+    }
+}
