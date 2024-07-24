00042871b3c72f4536883dfd5c3bff9097e6f029
Challduck <dgs2288@naver.com>
2024-07-24 11:41:33 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 128 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("Algorithm", "204");
+        map.put("DataAnalysis", "207");
+        map.put("ArtificialIntelligence", "302");
+        map.put("CyberSecurity", "B101");
+        map.put("Network", "303");
+        map.put("Startup", "501");
+        map.put("TestStrategy", "105");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(map.get(br.readLine())).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
