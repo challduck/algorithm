e7417d8dcb11ba2451376b931d76211df15d8476
Challduck <dgs2288@naver.com>
2024-08-01 21:13:55 +0900

    [Bronze I] Title: 팰린드롬수, Time: 124 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -4,51 +4,52 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        while (true){
+
+        while (true) {
             String str = br.readLine();
-            Stack<Character> stack = new Stack<>();
-            boolean flag = true;
-            if (Integer.parseInt(str) == 0){
+
+            if (str.equals("0")) {
                 break;
             }
 
-            if (str.length() % 2 == 1){
-                for (int i = 0; i < str.length(); i++) {
-                    if (i == str.length() / 2){
-                        continue;
-                    }
+            if (str.length() % 2 == 0) {
+                String before = "";
+                String after = "";
 
-                    if (i < str.length() / 2){
-                        stack.push(str.charAt(i));
-                    } else {
-                        char ch = stack.pop();
-                        if (str.charAt(i) != ch){
-                            flag = false;
-                            break;
-                        }
-                    }
+                for (int i = 0; i < str.length() / 2; i++) {
+                    before += Character.toString(str.charAt(i));
+                }
+
+                for (int i = str.length() - 1; i >= str.length() / 2; i--) {
+                    after += Character.toString(str.charAt(i));
+                }
+
+                if (before.equals(after)) {
+                    sb.append("yes").append("\n");
+                } else {
+                    sb.append("no").append("\n");
                 }
             } else {
-                for (int i = 0; i < str.length(); i++) {
-                    if (i < str.length() / 2){
-                        stack.push(str.charAt(i));
-                    } else {
-                        char ch = stack.pop();
-                        if (str.charAt(i) != ch){
-                            flag = false;
-                            break;
-                        }
-                    }
-                }
-            }
+                String before = "";
+                String after = "";
 
-            if (flag){
-                sb.append("yes").append("\n");
-            }else {
-                sb.append("no").append("\n");
+                for (int i = 0; i < str.length() / 2; i++) {
+                    before += Character.toString(str.charAt(i));
+                }
+
+                for (int i = str.length() - 1; i > str.length() / 2; i--) {
+                    after += Character.toString(str.charAt(i));
+                }
+
+                if (before.equals(after)) {
+                    sb.append("yes").append("\n");
+                } else {
+                    sb.append("no").append("\n");
+                }
             }
         }
         System.out.println(sb);
