02925df4e7e5fa1bfd16451470bcd1c2631b1230
Challduck <dgs2288@naver.com>
2024-08-04 23:46:00 +0900

    [Bronze V] Title: 사파리월드, Time: 108 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        System.out.println(n > m ? Math.abs(m - n) : Math.abs(n - m));
+    }
+}
