d5a8b12357c78aec59505b8a042c687287b15ab9
Challduck <dgs2288@naver.com>
2024-08-06 21:01:27 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1792 ms, Memory: 156932 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -3,17 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
             pq.add(Integer.parseInt(br.readLine()));
         }
-        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
             sb.append(pq.poll()).append("\n");
