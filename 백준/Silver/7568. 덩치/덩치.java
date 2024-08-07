24e1f3f416efe9f47b320b8e7316e7d4dcc66ec8
Challduck <dgs2288@naver.com>
2024-08-07 21:07:57 +0900

    [Silver V] Title: 덩치, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -7,34 +7,31 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][2];
 
-        StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int weight = Integer.parseInt(st.nextToken());
-            int height = Integer.parseInt(st.nextToken());
-
-            arr[i][0] = weight;
-            arr[i][1] = height;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 0; i < arr.length; i++) {
-            int rank = 1;
+            int k = 1;
             for (int j = 0; j < arr.length; j++) {
                 if (i == j) {
                     continue;
                 }
                 
-                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]){
-                    rank++;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    k++;
                 }
             }
-            sb.append(rank).append(" ");
+            sb.append(k).append(" ");
         }
+
         System.out.println(sb);
     }
-
 }
