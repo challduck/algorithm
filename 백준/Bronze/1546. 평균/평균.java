086ec86e3da9ff2a36d650be62f7c9942cfaf8fa
Challduck <dgs2288@naver.com>
2024-08-08 15:48:29 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,29 +1,30 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int max = Integer.MIN_VALUE;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(reader.readLine());
-        String[] str = reader.readLine().split(" ");
-        List<Double> list = new ArrayList<>();
-        double sum = 0;
-        for (int i = 0; i < str.length; i++) {
-            list.add(Double.parseDouble(str[i]));
-        }
-        Collections.sort(list);
-        if (list.size() == 1) {
-            System.out.println(100);
-        } else {
-            for (int i = 0; i < list.size(); i++) {
-                sum += (double) list.get(i) / (double) list.get(list.size() - 1) * 100L;
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            arr[i] = a;
+            if (max < a) {
+                max = a;
             }
-            System.out.println(sum / (double) list.size());
         }
-    }
 
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += ((double) arr[i] / max) * 100;
+        }
+
+        System.out.println(sum / n);
+    }
 }
