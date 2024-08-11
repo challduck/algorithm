4d02936cb0a56781bfa6d0bb88fa4a0fbe398659
Challduck <dgs2288@naver.com>
2024-08-11 20:53:22 +0900

    [Silver IV] Title: 스택 2, Time: 1052 ms, Memory: 234628 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+
+            if (command == 1) {
+                stack.add(Integer.parseInt(st.nextToken()));
+            } else if (command == 2) {
+                if (!stack.isEmpty()) {
+                    sb.append(stack.pop()).append("\n");
+                } else {
+                    sb.append(-1).append("\n");
+                }
+            } else if (command == 3) {
+                sb.append(stack.size()).append("\n");
+            } else if (command == 4) {
+                sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+            } else if (command == 5) {
+                if (!stack.isEmpty()) {
+                    sb.append(stack.peek()).append("\n");
+                } else {
+                    sb.append(-1).append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
