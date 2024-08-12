29a96f9acb513fcd083a56a81d7f06a86d9493b7
Challduck <dgs2288@naver.com>
2024-08-12 09:37:03 +0900

    [Silver III] Title: 수열, Time: 268 ms, Memory: 24076 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + arr[i];
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = k; i < n + 1; i++) {
+            if (max < prefix[i] - prefix[i - k]) {
+                max = prefix[i] - prefix[i - k];
+            }
+        }
+        System.out.println(max);
+    }
+}
