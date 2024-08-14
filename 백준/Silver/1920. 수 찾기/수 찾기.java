734ea4963f9a902065d3bebca3c85105aa9fed6c
Challduck <dgs2288@naver.com>
2024-08-14 21:05:21 +0900

    [Silver IV] Title: 수 찾기, Time: 596 ms, Memory: 52080 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,23 +1,33 @@
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Set<Integer> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+
         int n = Integer.parseInt(br.readLine());
-        String[] arr1 = br.readLine().split(" ");
+        Map<Integer, Integer> map = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            set.add(Integer.parseInt(arr1[i]));
+            int a = Integer.parseInt(st.nextToken());
+            map.put(a, map.getOrDefault(a, 0) + 1);
         }
+
         int m = Integer.parseInt(br.readLine());
-        String[] arr2 = br.readLine().split(" ");
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            System.out.println(set.contains(Integer.parseInt(arr2[i])) ? 1 : 0);
+            if (map.containsKey(Integer.parseInt(st.nextToken()))) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
         }
+
+        System.out.println(sb);
+
     }
 }
