42bf46ecaa8a52c2fbc6279f566630f32c41a644
Challduck <dgs2288@naver.com>
2024-08-16 21:11:10 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -6,8 +6,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
 
         int temp = n % 5;
         int bag = n / 5;
