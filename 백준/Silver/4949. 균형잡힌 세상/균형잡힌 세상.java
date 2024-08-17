94fc309bdbd199111367301c1c30418d19cd4136
Challduck <dgs2288@naver.com>
2024-08-17 21:04:42 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 164 ms, Memory: 19224 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -4,58 +4,34 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        while (true) {
-            Stack<Character> stack = new Stack<>();
-            String str = br.readLine();
+        String str;
+        while (!(str = br.readLine()).equals(".")) {
+            sb.append(isBalanced(str) ? "yes" : "no").append("\n");
+        }
 
-            if (str.equals(".")) {
-                break;
-            }
+        System.out.print(sb);
+    }
 
-            boolean flag = true;
-            for (int i = 0; i < str.length(); i++) {
-                char ch = str.charAt(i);
-                if (ch == '(' || ch == '[') {
-                    stack.add(ch);
-                } else if (ch == ')') {
-                    if (stack.isEmpty()){
-                        flag = false;
-                        break;
-                    } else {
-                        if (stack.peek() == '(') {
-                            stack.pop();
-                        } else {
-                            flag = false;
-                            break;
-                        }
-                    }
-                } else if (ch == ']') {
-                    if (stack.isEmpty()){
-                        flag = false;
-                        break;
-                    } else {
-                        if (stack.peek() == '[') {
-                            stack.pop();
-                        } else {
-                            flag = false;
-                            break;
-                        }
-                    }
-                } else {
-                    continue;
+    private static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == ']') {
+                if (stack.isEmpty() || !isPair(stack.pop(), ch)) {
+                    return false;
                 }
             }
-
-            if (stack.isEmpty() && flag) {
-                sb.append("yes").append("\n");
-            } else {
-                sb.append("no").append("\n");
-            }
         }
-        System.out.println(sb);
+        return stack.isEmpty();
+    }
+
+    private static boolean isPair(char open, char close) {
+        return (open == '(' && close == ')') || (open == '[' && close == ']');
     }
 }
