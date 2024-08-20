eaad7774fcfec1df2f33b88ca0052f2e9bf99e35
Challduck <dgs2288@naver.com>
2024-08-20 20:56:31 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 992 ms, Memory: 177428 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,34 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        Map<String, Integer> map = new HashMap<>();
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int idx = 0;
-        while (st.hasMoreTokens()) {
-            String str = st.nextToken();
-            map.put(str, map.getOrDefault(str, 0) + 1);
-        }
-
-        int m = Integer.parseInt(br.readLine());
-
+import java.io.BufferedReader;  
+import java.io.IOException;  
+import java.io.InputStreamReader;  
+import java.util.*;  
+  
+public class Main {  
+    public static void main(String[] args) throws IOException {  
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
         StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+        int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            String str = st.nextToken();
-            if (map.get(str) == null) {
-                sb.append("0 ");
+        for (int i = 0; i < M; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if (map.containsKey(a)) {
+                sb.append(map.get(a)).append(" ");
             } else {
-                sb.append(map.get(str) + " ");
+                sb.append(0).append(" ");
             }
         }
         System.out.println(sb);
-    }
+    }  
 }
