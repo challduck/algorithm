413ea5c91c17bcd2defcd9cdcabd50c338666469
Challduck <dgs2288@naver.com>
2024-08-21 16:13:44 +0900

    [Gold IV] Title: 오큰수, Time: 908 ms, Memory: 142860 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -7,12 +7,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
         Stack<Integer> stack = new Stack<>();
-        int[] OKeunSu = new int[n];
-        Arrays.fill(OKeunSu, -1);
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
+
+        int[] OkeunSu = new int[n];
+        Arrays.fill(OkeunSu, -1);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < n; i++) {
             int a = Integer.parseInt(st.nextToken());
@@ -20,16 +22,19 @@ public class Main {
         }
 
         for (int i = n - 1; i >= 0; i--) {
+            // 1번째 조건문
             while (!stack.isEmpty() && stack.peek() <= arr[i]) {
                 stack.pop();
             }
+            // 2번째 조건문
             if (!stack.isEmpty()) {
-                OKeunSu[i] = stack.peek();
+                OkeunSu[i] = stack.peek();
             }
+            // 3번째
             stack.push(arr[i]);
         }
 
-        for (int i : OKeunSu) {
+        for (int i : OkeunSu) {
             sb.append(i).append(" ");
         }
         System.out.println(sb);
