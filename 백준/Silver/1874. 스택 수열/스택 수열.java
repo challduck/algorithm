4157acbfe9989e063cc17b0e49a55fea5254e695
Challduck <dgs2288@naver.com>
2024-08-21 15:09:53 +0900

    [Silver II] Title: 스택 수열, Time: 304 ms, Memory: 27900 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,19 +1,19 @@
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
         int temp = 1;
 
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
+
             while (temp <= num) {
                 stack.push(temp);
                 sb.append("+").append("\n");
