9b16bf4ff060d637320c343ed8572f12823a49f2
Challduck <dgs2288@naver.com>
2024-08-22 21:04:52 +0900

    [Silver III] Title: 소수 구하기, Time: 240 ms, Memory: 24252 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,22 +1,23 @@
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> list = primeList(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        List<Integer> list = prime(m, n);
         for (Integer integer : list) {
             sb.append(integer).append("\n");
         }
         System.out.println(sb);
     }
 
-    private static List<Integer> primeList(int start, int end) {
+    private static List<Integer> prime(int start, int end) {
         boolean[] isPrime = new boolean[end + 1];
 
         for (int i = 2; i <= end; i++) {
