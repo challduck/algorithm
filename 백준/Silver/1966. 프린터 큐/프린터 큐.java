e3557b91c0746862706f556ca565ad8a8bca7f09
Challduck <dgs2288@naver.com>
2024-08-23 21:16:28 +0900

    [Silver III] Title: 프린터 큐, Time: 124 ms, Memory: 15032 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,55 +1,58 @@
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < num; i++) {
-            String[] docs = br.readLine().split(" ");
-            int docs_length = Integer.parseInt(docs[0]);
-            int docs_index = Integer.parseInt(docs[1]);
-            String[] list = br.readLine().split(" ");
+        for (int i = 0; i < t; i++) {
             Queue<Docs> queue = new LinkedList<>();
-            
-            for (int j = 0; j < docs_length; j++) {
-                queue.add(new Docs(j, Integer.parseInt(list[j])));
-            }
-            int order = 0;
-            while (!queue.isEmpty()) {
-                Docs current = queue.poll();
-                boolean is_high_sequence = false;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
 
-                for (Docs doc : queue) {
-                    if (doc.sequence > current.sequence) {
-                        is_high_sequence = true;
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                queue.add(new Docs(j, Integer.parseInt(st.nextToken())));
+            }
+
+            int order = 0;
+
+            while (!queue.isEmpty()) {
+                Docs currentDocs = queue.poll();
+                boolean flag = false;
+
+                for (Docs docs : queue) {
+                    if (docs.sequence > currentDocs.sequence) {
+                        flag = true;
                         break;
                     }
                 }
 
-                if (is_high_sequence) {
-                    queue.add(current);
+                if (flag) {
+                    queue.add(currentDocs);
                 } else {
                     order++;
-                    if (current.index == docs_index) {
-                        System.out.println(order);
+                    if (currentDocs.index == m) {
+                        sb.append(order).append("\n");
                         break;
                     }
                 }
             }
         }
-
-        
+        System.out.println(sb);
     }
 }
 
 class Docs {
     int index;
     int sequence;
-    Docs(int index, int sequence){
+
+    Docs(int index, int sequence) {
         this.index = index;
         this.sequence = sequence;
     }
