60d825a08103e3727a53438929b0d0dc6ef5ab56
Challduck <dgs2288@naver.com>
2024-08-25 20:12:37 +0900

    [Silver V] Title: 돌 게임, Time: 100 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        System.out.println(Integer.parseInt(br.readLine()) % 2 == 1 ? "SK" : "CY");
+    }
+}
