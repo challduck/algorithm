efd1b2c02ddefbfc9b8c955aa1039e99e0772aac
Challduck <dgs2288@naver.com>
2024-08-31 21:03:09 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -2,22 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
+
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[n - 1 - i] = Integer.parseInt(br.readLine());
+            int a = Integer.parseInt(br.readLine());
+            arr[i] = a;
         }
         int cnt = 0;
-        for(int num : arr) {
-            if (k >= num){
-                cnt += k / num;
-                k = k % num;
+        int idx = arr.length - 1;
+        while (true) {
+            if (k == 0) {
+                break;
             }
+
+            if (k / arr[idx] == 0) {
+                idx--;
+            }
+            cnt += k / arr[idx];
+            k %= arr[idx];
         }
         System.out.println(cnt);
     }
