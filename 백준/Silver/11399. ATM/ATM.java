69d3bdb7655118f1f8afe04c691126a605b0432b
Challduck <dgs2288@naver.com>
2024-09-02 21:12:45 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -4,23 +4,25 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
         Arrays.sort(arr);
-        int sum = 0;
         int temp = 0;
+        int sum = 0;
         for (int i = 0; i < n; i++) {
-            sum = sum + arr[i];
-            temp += sum;
+            int prev = temp;
+            temp += (temp + arr[i]) - prev;
+            sum += prev;
         }
-        System.out.println(temp);
+        System.out.println(sum + temp);
     }
 }
