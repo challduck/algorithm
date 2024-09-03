87f343d9d2aab859837bf64ca8308535f0ab8ba9
Challduck <dgs2288@naver.com>
2024-09-03 20:56:16 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 524 ms, Memory: 55716 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -4,24 +4,23 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         Map<String, String> map = new HashMap<>();
-
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            map.put(st.nextToken(), st.nextToken());
+            String site = st.nextToken();
+            String pw = st.nextToken();
+            map.put(site, pw);
         }
-
         StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            sb.append(map.get(st.nextToken())).append("\n");
+            sb.append(map.get(br.readLine())).append("\n");
         }
-        System.out.println(sb.deleteCharAt(sb.length() - 1));
+        System.out.println(sb);
     }
 }
