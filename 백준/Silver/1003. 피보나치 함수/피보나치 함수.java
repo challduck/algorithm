8852d47cd7f9dc8ed7a29d0839e29b9ade19c860
Challduck <dgs2288@naver.com>
2024-09-04 21:18:58 +0900

    [Silver III] Title: 피보나치 함수, Time: 116 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -24,13 +24,8 @@ public class Main {
     static void fibonacci() {
         zero[0] = 1;
         one[0] = 0;
-        // zero[1] = 0;
-        // one[1] = 1;
-
         for (int i = 1; i <= 40; i++) {
-            // zero[i] = zero[i - 1] + zero[i - 2];
             zero[i] = one[i - 1];
-            // one[i] = one[i - 1] + one[i - 2];
             one[i] = zero[i - 1] + zero[i];
         }
     }
