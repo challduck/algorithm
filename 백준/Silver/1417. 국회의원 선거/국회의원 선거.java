834801c742b899909ce36ae6e8b886e7c7487cd3
Challduck <dgs2288@naver.com>
2024-09-05 13:24:05 +0900

    [Silver V] Title: 국회의원 선거, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -4,20 +4,22 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        int dasom_vote_num = Integer.parseInt(br.readLine());
+        int dasom = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < n - 1; i++) {
             pq.add(Integer.parseInt(br.readLine()));
         }
+
         int cnt = 0;
-        while (!pq.isEmpty() && pq.peek() >= dasom_vote_num) {
+        while (!pq.isEmpty() && pq.peek() >= dasom) {
             int max = pq.poll();
             max--;
-            dasom_vote_num++;
+            dasom++;
             cnt++;
             pq.add(max);
         }
