fc2ac3fd5d2d2bc83c9ea558bef358119ea4d990
Challduck <dgs2288@naver.com>
2024-09-06 17:51:41 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -6,46 +6,44 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        
-        List<Integer>[] graph = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++) {
-            graph[i] = new ArrayList<>();
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            graph.put(i, new ArrayList<>());
         }
-        boolean[] visited = new boolean[N + 1];
-        
-        for (int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            graph[A].add(B);
-            graph[B].add(A);
+
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            graph.get(v1).add(v2);
+            graph.get(v2).add(v1);
         }
-        
-        int result = dfs(1, graph, visited);
-        
-        System.out.println(result);
+
+        System.out.println(bfs(n, 1, graph));
     }
-    
-    public static int dfs(int start, List<Integer>[] graph, boolean[] visited) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(start);
-        visited[start] = true;
-        int count = 0;
-        
-        while (!stack.isEmpty()) {
-            int current = stack.pop();
-            
-            for (int neighbor : graph[current]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    stack.push(neighbor);
-                    count++;
+
+    static int bfs(int n, int start, Map<Integer, List<Integer>> graph) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        boolean[] visited = new boolean[n + 1];
+
+        int cnt = -1;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            if (!visited[current]) {
+                visited[current] = true;
+                cnt++;
+                for (Integer i : graph.get(current)) {
+                    if (!visited[i]) {
+                        queue.add(i);
+                    }
                 }
             }
         }
-        
-        return count;
+        return cnt;
     }
 }
