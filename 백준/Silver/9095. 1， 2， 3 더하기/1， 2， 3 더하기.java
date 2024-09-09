58cf681d7364b01a6495531100773397d0f7e999
Challduck <dgs2288@naver.com>
2024-09-09 21:09:40 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -4,22 +4,52 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    /*
+     * n = 1
+     * 1
+     * 
+     * n = 2
+     * 1+1
+     * 2
+     * 
+     * n = 3
+     * 1+1+1
+     * 1+2
+     * 2+1
+     * 3
+     * 
+     * n = 4
+     * 1+1+1+1
+     * 1+1+2
+     * 1+2+1
+     * 2+1+1
+     * 2+2
+     * 1+3
+     * 3+1
+     * 
+     * 만약 idx n-1 n-2 n-3 의 합이라면, 5 = 13, 6 = 24 7 = 44
+     * 찾음
+     */
+    static int[] dp = new int[12];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-
+        int t = Integer.parseInt(br.readLine());
+        dp();
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            int[] arr = new int[n + 3];
-            arr[1] = 1;
-            arr[2] = 2;
-            arr[3] = 4;
-            for (int j = 4; j <= n; j++) {
-                arr[j] = arr[j - 1] + arr[j - 2] + arr[j - 3];
-            }
-            sb.append(arr[n] + "\n");
+            sb.append(dp[n - 1]).append("\n");
         }
         System.out.println(sb);
+    }
+
+    static void dp() {
+        dp[0] = 1;
+        dp[1] = 2;
+        dp[2] = 4;
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
     }
 }
