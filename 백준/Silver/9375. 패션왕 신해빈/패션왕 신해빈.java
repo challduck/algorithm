066ac5dade80da61dee414224da541c4bf03c661
Challduck <dgs2288@naver.com>
2024-09-10 21:18:07 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 96 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -4,24 +4,31 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int test_case = Integer.parseInt(br.readLine());
-        for (int i = 0; i < test_case; i++) {
-            int n = Integer.parseInt(br.readLine());
-            Map<String, Integer> map = new HashMap<>();
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for (int i = 0; i < t; i++) {
+            Map<String , Integer> map = new HashMap<>();
+            int n = Integer.parseInt(br.readLine());
             for (int j = 0; j < n; j++) {
-                String[] str = br.readLine().split(" ");
-                map.put(str[1], map.getOrDefault(str[1], 0) + 1);
+                st = new StringTokenizer(br.readLine());
+                String val = st.nextToken();
+                String key = st.nextToken();
+
+                map.put(key, map.getOrDefault(key, 0) + 1);
             }
 
             int answer = 1;
             for (int cnt : map.values()) {
                 answer *= (cnt + 1);
             }
+            sb.append(answer - 1).append("\n");
 
-            System.out.println(answer - 1);
         }
+
+        System.out.println(sb);
     }
 }
