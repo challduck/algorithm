0d32ef54bd5fd29a6952f3b0f2abd441f9c1d5a7
Challduck <dgs2288@naver.com>
2024-09-27 10:20:58 +0900

    [Bronze I] Title: 암호 키, Time: 196 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/1816. 암호 키/암호 키.java
+++ 백준/Bronze/1816. 암호 키/암호 키.java
@@ -3,21 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            long s = Long.parseLong(br.readLine());
-            boolean isBad = false;
+
+        for (int i = 0; i < t; i++) {
+            long num = Long.parseLong(br.readLine());
+            boolean flag = false;
+
             for (int j = 2; j <= 1000000; j++) {
-                if (s % j == 0) {
-                    isBad = true;
+                if (num % j == 0) {
+                    flag = true;
+                    break;
                 }
             }
-            if (isBad) {
+
+            if (flag) {
                 sb.append("NO").append("\n");
             } else {
                 sb.append("YES").append("\n");
