ea3348821ffd699b7be739d8dd844aa16afcae5f
Challduck <dgs2288@naver.com>
2024-09-27 10:46:46 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
@@ -6,14 +6,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // "3명"이 주어진 조건에서 가장 적절한 경우의 수 를 찾는 문제
         int n = Integer.parseInt(br.readLine());
         int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int j2 = 0; j2 < n; j2++) {
-                    if (i + j + j2 == n && i >= j + 2 && i != 0 && j != 0 && j2 != 0 && j2 % 2 == 0) {
+        // 목적 -> 분배하기 위한 적절한 "경우의 수"를 찾는 문제
+
+        for (int i = 1; i < n; i++) { // 택희
+            for (int j = 1; j < n; j++) { // 영훈
+                for (int k = 1; k < n; k++) { // 남규
+
+                    if (n - i - j - k == 0 && k - j >= 2 && i % 2 == 0) {
                         cnt++;
                     }
+
                 }
             }
         }
