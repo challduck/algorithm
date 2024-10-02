04b5d53d8df1ff140d82793832d19af14b29c323
Challduck <dgs2288@naver.com>
2024-10-02 21:20:11 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 23392 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -5,41 +5,38 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
         boolean[] visited = new boolean[100001];
-        
-        System.out.println(bfs(visited, N, K));
+
+        System.out.println(bfs(n, k, visited));
     }
 
-    private static int bfs (boolean[] visited, int start, int end){
-        
+    private static int bfs(int n, int k, boolean[] visited) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{start , 0});
-        visited[start] = true;
+        queue.add(new int[] { n, 0 });
+        visited[n] = true;
 
-        int answer = 0;
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
+            int cur_num = current[0];
+            int dist = current[1];
 
-            if (current[0] == end) {
-                answer = current[1];
-                break;
+            if (cur_num == k) {
+                return dist;
             }
 
-            int[] next_location = {current[0] - 1, current[0] + 1, current[0] * 2};
+            int[] next_location = { current[0] - 1, current[0] + 1, current[0] * 2 };
 
             for (int i : next_location) {
                 if (i >= 0 && i <= 100000 && !visited[i]) {
-                    queue.add(new int[]{i, current[1] + 1});
+                    queue.add(new int[] { i, current[1] + 1 });
                     visited[i] = true;
                 }
             }
         }
-        return answer;
+        return -1;
     }
 }
