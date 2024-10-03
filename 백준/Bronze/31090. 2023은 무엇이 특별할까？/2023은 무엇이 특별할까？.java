b84f049422c195a15674f0e4644269d02cc12124
Challduck <dgs2288@naver.com>
2024-10-03 10:05:17 +0900

    [Bronze IV] Title: 2023은 무엇이 특별할까?, Time: 116 ms, Memory: 14800 KB -BaekjoonHub

---
+++ 백준/Bronze/31090. 2023은 무엇이 특별할까？/2023은 무엇이 특별할까？.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            int mod = num % 100;
+            if ((num + 1) % mod == 0) {
+                sb.append("Good").append("\n");
+            } else {
+                sb.append("Bye").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
