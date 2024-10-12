a1f3a2465a136c6be20f82b986bbb69741716c58
Challduck <dgs2288@naver.com>
2024-10-12 22:00:54 +0900

    [Bronze IV] Title: 빵, Time: 108 ms, Memory: 14340 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.java
+++ 백준/Bronze/25377. 빵/빵.java
@@ -1,28 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        int time = Integer.MAX_VALUE;
+        int minTime = Integer.MAX_VALUE;
+        boolean flag = false;
+
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
             if (a <= b) {
-                time = Math.min(time, a);
+                flag = true;
+                minTime = Math.min(minTime, b);
             }
         }
 
-        if (time == Integer.MAX_VALUE) {
-            System.out.println(-1);
+        if (flag) {
+            System.out.println(minTime);
         } else {
-            System.out.println(time);
+            System.out.println(-1);
         }
     }
 }
