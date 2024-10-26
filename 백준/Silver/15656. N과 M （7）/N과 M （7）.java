09d7ddad6d87c9a89b3bcd1fbadab25d541d71e5
Challduck <dgs2288@naver.com>
2024-10-26 16:06:31 +0900

    [Silver III] Title: N과 M (7), Time: 592 ms, Memory: 123684 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        recur(0, new int[m]);
+        System.out.print(sb);
+    }
+
+    private static void recur(int depth, int[] num_arr) {
+        if (depth == m) {
+            for (int i = 0; i < num_arr.length; i++) {
+                sb.append(num_arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            num_arr[depth] = arr[i];
+            recur(depth + 1, num_arr);
+        }
+    }
+}
