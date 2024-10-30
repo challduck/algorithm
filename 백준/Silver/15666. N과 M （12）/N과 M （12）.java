9f7955297893fe6160f396c078d83c774ee0a15a
Challduck <dgs2288@naver.com>
2024-10-30 20:42:32 +0900

    [Silver II] Title: N과 M (12), Time: 128 ms, Memory: 16160 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        recur(0, new int[m]);
+        System.out.print(sb);
+    }
+
+    private static void recur(int depth, int[] num_arr) {
+        if (depth == m) {
+            for (int i = 0; i < num_arr.length; i++) {
+                sb.append(num_arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prevNum = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == prevNum) {
+                continue;
+            }
+            
+            num_arr[depth] = arr[i];
+            if (depth > 0 && num_arr[depth - 1] > num_arr[depth]) {
+                continue;
+            }
+
+            recur(depth + 1, num_arr);
+            prevNum = arr[i];
+        }
+    }
+}
