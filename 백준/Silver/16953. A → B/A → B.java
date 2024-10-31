de584a97e2f9680212e3d95d7b58073f0eece3b4
Challduck <dgs2288@naver.com>
2024-10-31 20:53:37 +0900

    [Silver II] Title: A → B, Time: 124 ms, Memory: 18100 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -2,32 +2,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         long b = Long.parseLong(st.nextToken());
 
-        int cnt = 1;
+        System.out.println(bfs(a, b));
+    }
 
-        while (a != b){
-            if (a > b) {
-                cnt = -1;
-                break;
+    private static long bfs(int start, long end) {
+        Queue<long[]> queue = new LinkedList<>();
+        queue.add(new long[] { start, 1 });
+        while (!queue.isEmpty()) {
+            long[] current = queue.poll();
+            long current_num = current[0];
+            long dist = current[1];
+
+            if (current_num == end) {
+                return dist;
             }
 
-            if (b % 10 == 1){
-                b /= 10;
-                cnt++;
-            } else if (b % 2 == 0) {
-                b /= 2;
-                cnt++;
-            } else {
-                cnt = -1;
-                break;
+            long num1 = current_num * 2;
+            if (num1 <= end) {
+                queue.add(new long[] { num1, dist + 1 });
+            }
+            long num2 = current_num * 10 + 1;
+            if (num2 <= end) {
+                queue.add(new long[] { num2, dist + 1 });
             }
         }
-        System.out.println(cnt);
+        return -1;
     }
 }
