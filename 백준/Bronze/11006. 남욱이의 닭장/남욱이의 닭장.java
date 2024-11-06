9f12962495c16460c84aa65f7b79d1876b107671
Challduck <dgs2288@naver.com>
2024-11-06 21:22:28 +0900

    [Bronze III] Title: 남욱이의 닭장, Time: 100 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/11006. 남욱이의 닭장/남욱이의 닭장.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            sb.append(m * 2 - n).append(" ").append(m - (m * 2 - n)).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
