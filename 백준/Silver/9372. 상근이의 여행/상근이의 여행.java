034b5cc913dedd454da5c7b1182f13ed684754f6
Challduck <dgs2288@naver.com>
2024-11-21 20:49:29 +0900

    [Silver IV] Title: 상근이의 여행, Time: 212 ms, Memory: 30844 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -12,49 +12,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < t; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-            visited = new boolean[n + 1];
-            graph = new HashMap<>();
-
-            for (int j = 1; j <= n; j++) {
-                graph.put(j, new ArrayList<>());
-            }
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
 
             for (int j = 0; j < m; j++) {
                 st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-
-                graph.get(a).add(b);
-                graph.get(b).add(a);
             }
 
-            sb.append(bfs()).append("\n");
+            sb.append(n - 1).append("\n");
         }
         System.out.print(sb.toString());
-    }
-
-    private static int bfs() {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        visited[1] = true;
-
-        int cnt = 0;
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-
-            for (int i : graph.get(current)) {
-                if (!visited[i]) {
-                    queue.add(i);
-                    visited[i] = true;
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
     }
 }
