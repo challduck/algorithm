1e6b1b40fb870c2710f6fff72768eb11ff19beef
Challduck <dgs2288@naver.com>
2024-12-22 10:37:03 +0900

    [Silver V] Title: CD, Time: 900 ms, Memory: 275704 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.java
+++ 백준/Silver/4158. CD/CD.java
@@ -1,39 +1,55 @@
-import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
-
             if (n == 0 && m == 0) {
                 break;
             }
-
-            Set<Integer> set = new HashSet<>();
+            int[] sangkeun = new int[n];
+            int[] sunyoung = new int[m];
 
             for (int i = 0; i < n; i++) {
-                set.add(Integer.parseInt(br.readLine()));
+                sangkeun[i] = Integer.parseInt(br.readLine());
+            }
+
+            for (int i = 0; i < m; i++) {
+                sunyoung[i] = Integer.parseInt(br.readLine());
             }
 
             int cnt = 0;
-
-            for (int i = 0; i < m; i++) {
-                if (set.contains(Integer.parseInt(br.readLine()))) {
+            for (int i : sangkeun) {
+                if (binarySearch(sunyoung, i)) {
                     cnt++;
                 }
             }
-
             sb.append(cnt).append("\n");
         }
+        System.out.print(sb);
+    }
 
-        System.out.print(sb.toString());
+    private static boolean binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return true;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
     }
 }
