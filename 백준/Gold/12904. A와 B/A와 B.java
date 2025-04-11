7c2742cf54b6bfa8272e2bb428cfcf49376e5432
YongSeok <dgs2288@naver.com>
2025-04-11 18:42:09 +0900

    [Gold V] Title: A와 B, Time: 112 ms, Memory: 14460 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        String T = br.readLine();
+
+        StringBuilder sb = new StringBuilder(T);
+
+        while (sb.length() > S.length()) {
+            if (sb.charAt(sb.length() - 1) == 'A') {
+                sb.deleteCharAt(sb.length() - 1);
+            } else if (sb.charAt(sb.length() - 1) == 'B') {
+                sb.deleteCharAt(sb.length() - 1);
+                sb.reverse();
+            }
+        }
+
+        if (sb.toString().equals(S)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
